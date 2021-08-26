3791c0ad287c4fe74c501a1cb28bd8affb47bd51
Grace Kendi <79199127+KendiGrace@users.noreply.github.com>
2021-08-26 20:12:06 +0300

    Update Transaction.kt

--- app/src/main/java/victorines_wallet/com/models/Transaction.kt
+++ app/src/main/java/victorines_wallet/com/models/Transaction.kt
@@ -1,3 +1,7 @@
 package victorines_wallet.com.models
 
-data class Transaction()
+data class Transaction(  var description:String,
+    var reference:String,
+    var date: String,
+    var Amount:Int,
+    var type:String)
